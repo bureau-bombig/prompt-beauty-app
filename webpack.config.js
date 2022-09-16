a56cbea1a182b11a472dc60a8d6790bddf7ad6ab
denizbinay <deniz@bombig.net>
2022-09-16 20:55:17 +0200

    add webpack conf and clean dependencies

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    // Add your plugins here
+  ],
+  module: {
+    // Add your Modules here
+  },
+};
