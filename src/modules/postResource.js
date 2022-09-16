61a2a568016e8c4638457284fb2008da19a2796c
denizbinay <deniz@bombig.net>
2022-09-16 07:06:48 +0200

    remove easymde from bundle and change easymde settings

--- src/modules/postResource.js
+++ src/modules/postResource.js
@@ -1,7 +1,13 @@
-import EasyMDE from "easymde";
+// import EasyMDE from "easymde";
+// Embedded via unpkg only on post reource site. already loaded.
 
 async function postResource() {
-  const easyMDE = new EasyMDE({ element: document.getElementById("bb_easymde_editor") });
+  const easyMDE = new EasyMDE({
+    autoDownloadFontAwesome: false,
+    element: document.getElementById("bb_easymde_editor"),
+    initialValue:
+      "# EasyMDE \nGo ahead, play around with the editor! Be sure to check out **bold**, *italic* and ~~strikethrough~~ styling, [links](https://google.com) and all the other features. You can type the Markdown syntax, use the toolbar, or use shortcuts like `ctrl-b` or `cmd-b`.",
+  });
   console.log("easyMDE", easyMDE);
   console.log("easyMDE.value()", easyMDE.value());
 }
