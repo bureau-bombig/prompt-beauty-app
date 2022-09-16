ce13e8135b89c3b8483e9aa46cf36f3decb91441
denizbinay <deniz@bombig.net>
2022-09-16 02:28:15 +0200

    run script after DOMContentloaded

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import postImage from "./modules/postImage.js";
-
 console.log("Welcome to Prompt Beauty");
 
-if (top.location.pathname.toString() === "/post-image/") {
-  console.log("Post Image page detected");
-  postImage();
-}
+onDOMContentLoaded = (event) => {
+  // Post Image Script
+  if (top.location.pathname.toString() === "/post-image/") {
+    console.log("Post Image page detected");
+    postImage();
+  }
+
+  // Edit Image Script
+  // ...
+
+  // Edit Profile Script
+  // ...
+
+  // Post Resource Script
+  // ...
+};
