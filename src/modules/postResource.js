e822b0f1557aacc889f62ae5ebe87955f310972a
denizbinay <deniz@bombig.net>
2022-09-16 07:13:44 +0200

    change options

--- src/modules/postResource.js
+++ src/modules/postResource.js
@@ -3,8 +3,27 @@
 
 async function postResource() {
   const easyMDE = new EasyMDE({
-    autoDownloadFontAwesome: false,
     element: document.getElementById("bb_easymde_editor"),
+    showIcons: [
+      "strikethrough",
+      "code",
+      "table",
+      "redo",
+      "heading",
+      "undo",
+      "heading-bigger",
+      "heading-smaller",
+      "heading-1",
+      "heading-2",
+      "heading-3",
+      "clean-block",
+      "horizontal-rule",
+    ],
+    autosave: {
+      enabled: true,
+      delay: 1000,
+      uniqueId: "mde-autosave-demo",
+    },
     initialValue:
       "# EasyMDE \nGo ahead, play around with the editor! Be sure to check out **bold**, *italic* and ~~strikethrough~~ styling, [links](https://google.com) and all the other features. You can type the Markdown syntax, use the toolbar, or use shortcuts like `ctrl-b` or `cmd-b`.",
   });
