dae35a91fd73ec63a0d70ee55a2657584323a4d6
denizbinay <deniz@bombig.net>
2022-09-16 06:05:54 +0200

    remove debug logs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import postImage from "./modules/postImage.js";
-console.log("Welcome to Prompt Beauty");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   // Post Image Script
   if (top.location.pathname.toString() === "/post-image/") {
-    console.log("Post Image page detected");
     postImage();
   }
 
