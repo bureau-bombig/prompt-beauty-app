824cc64de1a86a459be21de30fcd5ea1a3e7d51d
denizbinay <deniz@bombig.net>
2022-09-18 11:32:29 +0200

    change to complete import

--- src/modules/convertMarkdown.js
+++ src/modules/convertMarkdown.js
@@ -2,8 +2,8 @@
 // import { sanitize } from "dompurify";
 
 async function convertMarkdown() {
-  const { marked } = await import(/* webpackChunkName: "marked" */ "marked");
-  const { sanitize } = await import(/* webpackChunkName: "dompurify" */ "dompurify");
+  const marked = await import(/* webpackChunkName: "marked" */ "marked");
+  const dompurify = await import(/* webpackChunkName: "dompurify" */ "dompurify");
 
   const markdowns = document.querySelectorAll(".bb_markdown_content span");
   if (!markdowns) {
@@ -11,7 +11,7 @@ async function convertMarkdown() {
   }
   markdowns.forEach((markdown) => {
     const html = marked(markdown.textContent);
-    markdown.innerHTML = sanitize(html);
+    markdown.innerHTML = dompurify.sanitize(html);
   });
 }
 
