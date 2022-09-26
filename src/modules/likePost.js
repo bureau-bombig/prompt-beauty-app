233b3ca58e5edc542d55174927258fdaf1476b37
denizbinay <deniz@bombig.net>
2022-09-26 16:28:51 +0200

    refactor like logic

--- src/modules/likePost.js
+++ src/modules/likePost.js
@@ -8,32 +8,11 @@ async function likePost() {
     return;
   }
 
-  console.log(wp_api_settings);
-
-  // Check there is a user nonce
-  if (!wp_api_settings.nonce) {
-    return;
-  }
-
-  // Check User has liked this Post and Set liked class
-  const url = wp_api_settings.root + "promptbeauty/v1/like/";
-  const request = {
-    method: "GET",
-    headers: {
-      "X-WP-Nonce": wp_api_settings.nonce,
-    },
-  };
-  const response = await fetch(url, request);
-  const data = await response.json();
-  if (data.userHasLiked) {
-    bbLikeCounterWrapper.classList.add("bb-is-liked");
-  }
-
   // Add Event Listener to Like Button
   bbLikeButton.addEventListener("click", async (event) => {
     const { default: Notiflix } = await import(/* webpackChunkName: "notiflix" */ "notiflix");
 
-    if (!wp_api_settings.nonce) {
+    if (wp_api_settings.id == 0) {
       Notiflix.Notify.failure("Liking is only available for logged in users.");
       return;
     }
@@ -64,6 +43,26 @@ async function likePost() {
 
     Notiflix.Notify.failure("Something is Rotten in the State of Denmark");
   });
+
+  // Check there is a user nonce
+  if (wp_api_settings.id == 0) {
+    return;
+  }
+
+  // Check User has liked this Post and Set liked class
+  const url = wp_api_settings.root + "promptbeauty/v1/like/";
+  const request = {
+    method: "GET",
+    headers: {
+      "X-WP-Nonce": wp_api_settings.nonce,
+    },
+  };
+  const response = await fetch(url, request);
+  const data = await response.json();
+  if (data.userHasLiked) {
+    bbLikeCounterWrapper.classList.add("bb-is-liked");
+  }
 }
+
 likePost();
 export default likePost;
